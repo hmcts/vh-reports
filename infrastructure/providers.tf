4fac804d67c529b0ea0fc9a5b9d70c507ea77630
lowflying <chriscm.neu@gmail.com>
2022-04-04 12:23:44 +0100

    fixing backend

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,4 +1,13 @@
 terraform {
+  
+  backend "azurerm" {
+      resource_group_name  = "vh-core-infra-dev"
+      storage_account_name = "vhmanagementdev"
+      container_name       = "tfstate"
+      key                  = "vh-reporting/dev-terraform.tfstate"
+  }
+
+  required_version = "= 3.0.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
