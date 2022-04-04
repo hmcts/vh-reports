bea08854ea7c2795d528e85a4c364c31c0a9b8cf
lowflying <chriscm.neu@gmail.com>
2022-04-04 12:32:48 +0100

    fmt

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,10 +1,10 @@
 terraform {
-  
+
   backend "azurerm" {
-      resource_group_name  = "vh-core-infra-dev"
-      storage_account_name = "vhmanagementdev"
-      container_name       = "tfstate"
-      key                  = "vh-reporting/dev-terraform.tfstate"
+    resource_group_name  = "vh-core-infra-dev"
+    storage_account_name = "vhmanagementdev"
+    container_name       = "tfstate"
+    key                  = "vh-reporting/dev-terraform.tfstate"
   }
   required_providers {
     azurerm = {
