4ae4c5f61bde839b20bf14ab6bbedb2ea0f96515
lowflying <chriscm.neu@gmail.com>
2022-04-08 21:01:34 +0100

    moving linked service and adf pipeline deployments to TF

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -39,6 +39,7 @@ resource "azurerm_data_factory" "adf" {
 #  location            = azurerm_resource_group.vh-reporting-rg.location
 #  resource_group_name = azurerm_resource_group.vh-reporting-rg.name
 #}
+
 #TODO remove the below and use azurerm_api_connection to configure the connections and the json deploy task to configure the workflow
 resource "azurerm_template_deployment" "workflow" {
   resource_group_name = azurerm_resource_group.vh-reporting-rg.name
@@ -82,10 +83,135 @@ resource "azurerm_key_vault_access_policy" "kvaccess" {
 
 }
 
+resource "azurerm_data_factory_linked_service_key_vault" "adfkeyvault" {
+  name            = "vhreporting-kv-link-${var.env}"
+  data_factory_id = azurerm_data_factory.adf.id
+  key_vault_id    = data.azurerm_key_vault.core-kv.id
+}
+
+resource "azurerm_data_factory_linked_service_azure_blob_storage" "adfblob" {
+  name              = "vhreporting-blob-link-${var.env}"
+  data_factory_id   = azurerm_data_factory.adf.id
+  connection_string = data.azurerm_storage_account.core-sa.primary_connection_string
+}
+
+resource "azurerm_data_factory_linked_service_azure_sql_database" "adfvideodb" {
+  name              = "vhvideo-db"
+  data_factory_id   = azurerm_data_factory.adf.id
+  key_vault_connection_string {
+    linked_service_name = azurerm_data_factory_linked_service_key_vault.adfkeyvault.name
+    secret_name = "VhVideoDatabaseConnectionString"
+  }
+}
+
+resource "azurerm_data_factory_linked_service_azure_sql_database" "adfreportingdb" {
+  name              = "vhreporting-db"
+  data_factory_id   = azurerm_data_factory.adf.id
+  key_vault_connection_string {
+    linked_service_name = azurerm_data_factory_linked_service_key_vault.adfkeyvault.name
+    secret_name = "VhReportingDatabaseConnectionString"
+  }
+}
 resource "azurerm_storage_container" "vhreporting" {
   name                  = "vhreporting"
   storage_account_name  = data.azurerm_storage_account.core-sa.name
   container_access_type = "private"
+}
+
+resource "azurerm_data_factory_pipeline" "MasterStaging" {
+  name            = "MasterStaging"
+  data_factory_id = azurerm_data_factory.adf.id
+  variables = {}
+  activities_json = <<JSON
+   [
+			{
+				"name": "GetConfig",
+				"type": "Lookup",
+				"dependsOn": [],
+				"policy": {
+					"timeout": "7.00:00:00",
+					"retry": 0,
+					"retryIntervalInSeconds": 30,
+					"secureOutput": false,
+					"secureInput": false
+				},
+				"userProperties": [],
+				"typeProperties": {
+					"source": {
+						"type": "AzureSqlSource",
+						"sqlReaderStoredProcedureName": "[dbo].[sp_GetAdfConfig]",
+						"storedProcedureParameters": {
+							"MasterPipeline": {
+								"type": "String",
+								"value": "Master_ErrorFile"
+							}
+						},
+						"queryTimeout": "02:00:00",
+						"partitionOption": "None"
+					},
+					"dataset": {
+						"referenceName": "AsqlTarget",
+						"type": "DatasetReference",
+						"parameters": {
+							"SchemaName": "dbo",
+							"TableName": "AdfConfig"
+						}
+					},
+					"firstRowOnly": false
+				}
+			},
+			{
+				"name": "ForEach1",
+				"type": "ForEach",
+				"dependsOn": [
+					{
+						"activity": "GetConfig",
+						"dependencyConditions": [
+							"Succeeded"
+						]
+					}
+				],
+				"userProperties": [],
+				"typeProperties": {
+					"items": {
+						"value": "@activity('GetConfig').output.value",
+						"type": "Expression"
+					},
+					"isSequential": false,
+					"batchCount": 4,
+					"activities": [
+						{
+							"name": "StageAndLoad",
+							"type": "ExecutePipeline",
+							"dependsOn": [],
+							"userProperties": [],
+							"typeProperties": {
+								"pipeline": {
+									"referenceName": "Child_StageAppInsightsError",
+									"type": "PipelineReference"
+								},
+								"waitOnCompletion": false,
+								"parameters": {
+									"DeltaDateTime": {
+										"value": "@{item().DeltaValue}",
+										"type": "Expression"
+									},
+									"TableName": {
+										"value": "@{item().TableName}",
+										"type": "Expression"
+									},
+									"ProcedureName": {
+										"value": "@{item().ProcedureName}",
+										"type": "Expression"
+									}
+								}
+							}
+						}
+					]
+				}
+			}
+		],
+  JSON
 }
 
 
