0c3bbe6baee70b6483230c41a5452745948c6395
lowflying <chriscm.neu@gmail.com>
2022-04-11 11:33:46 +0100

    added all DB scripts to run

--- database/00_init.sql
+++ database/00_init.sql
@@ -3,3 +3,15 @@
 GO
 :r "D:\a\1\s\database\02_AdfConfig.sql"
 GO
+:r "D:\a\1\s\database\03_Calendar_Data.sql"
+GO
+:r "D:\a\1\s\database\04_Time_Data.sql"
+GO
+:r "D:\a\1\s\database\05_ReportingEnums.sql"
+GO
+:r "D:\a\1\s\database\06_reporting_views.sql"
+GO
+:r "D:\a\1\s\database\07_merge_procs.sql"
+GO
+:r "D:\a\1\s\database\08_stg_sp_TruncateTable.sql"
+GO
