ab1e22ef5697541adf137c37842bfeb549802ad7
lowflying <chriscm.neu@gmail.com>
2022-04-13 19:11:31 +0100

    force last good plan

--- database/enable_auto_tune.sql
+++ database/enable_auto_tune.sql
@@ -1,2 +1,3 @@
 -- Enable automatic tuning on an individual database
 ALTER DATABASE current SET AUTOMATIC_TUNING (CREATE_INDEX = ON);
+ALTER DATABASE current SET AUTOMATIC_TUNING ( FORCE_LAST_GOOD_PLAN = ON ); 
