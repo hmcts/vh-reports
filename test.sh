86139431806dfcf98cceccc744148267348b0be1
Chris C Moynihan <chriscm.neu@gmail.com>
2022-04-25 10:19:07 +0100

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for file in ./dataset/*
-do
-  fileName="$(basename $file .json)"
-  echo $fileName
-  az datafactory dataset create --resource-group vh-reporting-infra-dev --factory-name "vh-datafactory-dev" --name $fileName --properties $file --debug
-done
-          
