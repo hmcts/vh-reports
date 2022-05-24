21bd4cc20e04d8f83784d4294ce63beeded1f3bc
Thomas Watson <thomas.watson@version1.com>
2022-05-24 12:30:44 +0100

    Added more descriptions to variables

--- infrastructure/inputs.tf
+++ infrastructure/inputs.tf
@@ -1,12 +1,15 @@
 variable "env" {
+  description = "The environment the infrastructure will be created in"
   type = string
 }
 
 variable "location" {
+  description = "The Azure region the infrastructure will be created in"
   type = string
 }
 
 variable "administrator_login" {
+  description = "The user name for the database connections"
   type    = string
   default = "hvhearingsapiadmin"
 }
